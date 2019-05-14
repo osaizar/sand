6e216da77d9ff487ed7e672a309de8392fc674c9
ainhoa.achon <ainhoa.achon@opendeusto.es>
2019-05-14 19:58:46 +0200

    Now it's a little bit prettier :)

--- client/client.py
+++ client/client.py
@@ -3,10 +3,11 @@ import time
 from bitstring import BitArray
 
 HOST = 'localhost'
-PORT = 5553	# Arbitrary non-privileged port
+PORT = 5553  # Arbitrary non-privileged port
 
 ITERATIONS = 20
 PACKET_SIZE = 1024
+
 
 def get_send_rate(n):
     if n == "0":
@@ -14,45 +15,48 @@ def get_send_rate(n):
     else:
         return 1024 * 100
 
+
 def seg_to_bs(numSeconds, sendRate):
-   return numSeconds*sendRate
+    return numSeconds*sendRate
+
 
 def bs_to_seg(numBytes, sendRate):
-   return float(numBytes)/sendRate
+    return float(numBytes)/sendRate
 
 
 def send_file(file_bytes):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((HOST,PORT))
+    sock.connect((HOST, PORT))
 
     # We'll add to this tally as we send() bytes, and subtract from
     # at the schedule specified by (sendRate)
-    bytesAheadOfSchedule = 0
+    # bytesAheadOfSchedule = 0
 
     # Dummy data buffer, just for testing
     dataBuf = bytearray(PACKET_SIZE)
 
-    prevTime = None
+    # prevTime = None
 
     for b in file_bytes:
         sendRate = get_send_rate(b)
         print ("[DEBUG] sending "+b+" Rate "+str(sendRate/1024)+" kb/s")
 
         for i in range(ITERATIONS):
-           now = time.time()
-           numBytesSent = sock.send(dataBuf)
-           after = time.time()
-           send_time = after - now
+            now = time.time()
+            numBytesSent = sock.send(dataBuf)
+            after = time.time()
+            send_time = after - now
 
-           if numBytesSent > 0:
-               ideal_send_time = bs_to_seg(numBytesSent, sendRate)
-               sleep_time = ideal_send_time - send_time
-               if sleep_time > 0:
-                   time.sleep(sleep_time)
+            if numBytesSent > 0:
+                ideal_send_time = bs_to_seg(numBytesSent, sendRate)
+                sleep_time = ideal_send_time - send_time
+                if sleep_time > 0:
+                    time.sleep(sleep_time)
 
-           else:
-              print ("[!] Error sending data, exiting!")
-              break
+            else:
+                print ("[!] Error sending data, exiting!")
+                break
+
 
 def main():
     in_file = open("test.txt", "rb")
@@ -62,6 +66,7 @@ def main():
     print ("[+] Got file, sending")
     file_bytes = BitArray(data).bin
     send_file(file_bytes)
+
 
 if __name__ == '__main__':
     main()
--- server/server.py
+++ server/server.py
@@ -5,21 +5,24 @@ import threading
 from bitstring import BitArray
 from statistics import median_high
 
-HOST = ''	# Symbolic name, meaning all available interfaces
-PORT = 5553	# Arbitrary non-privileged port
+HOST = ''  # Symbolic name, meaning all available interfaces
+PORT = 5553  # Arbitrary non-privileged port
 
 ITERATIONS = 20
 PACKET_SIZE = 1024
 
+
 def to_file(in_bits):
     in_bits = BitArray(bin=in_bits)
     open("out", "wb").write(in_bits.tobytes())
+
 
 def translate(bs):
     if bs < 70:
         return "0"
     else:
         return "1"
+
 
 def client_thread(conn, addr):
     in_bits = ""
@@ -44,34 +47,36 @@ def client_thread(conn, addr):
     print ("[+] Ended connection with " + addr[0] + ":" + str(addr[1]))
     to_file(in_bits)
 
+
 def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print('[+] Socket created')
 
-    #Bind socket to local host and port
+    # Bind socket to local host and port
     try:
-    	s.bind((HOST, PORT))
+        s.bind((HOST, PORT))
     except socket.error as msg:
-    	print("[!] Bind failed. Error Code : " + str(msg[0]) + " Message " + msg[1])
-    	sys.exit()
+        print("[!] Bind failed. Error Code : " + str(msg[0]) + " Message "
+              + msg[1])
+        sys.exit()
 
     print ("[+] Socket bind complete")
 
-    #Start listening on socket
+    # Start listening on socket
     s.listen(10)
     print ("[+] Socket now listening")
 
-    #now keep talking with the client
+    # Now keep talking with the client
     try:
         while True:
-            #wait to accept a connection - blocking call
+            # Wait to accept a connection - blocking call
             conn, addr = s.accept()
             print ("[+] Connected with " + addr[0] + ":" + str(addr[1]))
 
             threading.Thread(target=client_thread,
-                    args=(conn, addr),
-                    ).start()
-    except:
+                             args=(conn, addr),).start()
+    except Exception as e:
+        print("[!] Exception: " + str(e))
         print("[!] Closing")
 
     s.close()
