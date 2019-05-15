fdd0759300dba07417feb1542207acb8f741b800
osaizar <oierlas@gmail.com>
2019-05-15 19:15:15 +0200

    get pokemon

--- client/__init__.py
+++ client/__init__.py
@@ -1,16 +1,17 @@
 from libclient import *
+from random import randint
 
 def get_pokemon():
-    pass
+    return "pokemon/"+str(randint(0,5757))+".png"
 
 def main():
     data = open("test.txt", "rb").read()
-    covert = get_pokemon()
+    covert = open(get_pokemon(), "rb").read()
 
     print ("[+] Got file, sending")
-    
+
     file_in_bits = BitArray(data).bin
-    send_file(file_in_bits, covert)
+    send_file(file_in_bits, None) # TODO: add covert
 
 
 if __name__ == '__main__':
