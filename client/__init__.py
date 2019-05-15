34b1623c18a3884a378d830e033234a8ec2217a8
osaizar <oierlas@gmail.com>
2019-05-15 14:10:10 +0200

    get pokemons

--- client/__init__.py
+++ client/__init__.py
@@ -1,11 +1,16 @@
 from libclient import *
 
+def get_pokemon():
+    pass
+
 def main():
     data = open("test.txt", "rb").read()
+    covert = get_pokemon()
 
     print ("[+] Got file, sending")
+    
     file_in_bits = BitArray(data).bin
-    send_file(file_in_bits)
+    send_file(file_in_bits, covert)
 
 
 if __name__ == '__main__':
